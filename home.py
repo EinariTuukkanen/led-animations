50745c9d884e135f432031e9c4690ccb9791c022
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2018-07-17 23:03:48 +0300

    Adds home

---
+++ home.py
@@ -0,0 +1,41 @@
+from neopixel import ws, Adafruit_NeoPixel, Color
+
+import config
+
+
+class Strip(Adafruit_NeoPixel):
+    """ Extends Adafruits NeoPixel by adding set all pixels at once -method """
+    _prev_pixels = []
+
+    def set_pixels(self, pixels):
+        for i in range(len(pixels)):
+            if (len(self._prev_pixels) == len(pixels) and
+                    self._prev_pixels[i] == pixels[i]):
+                continue
+            self.setPixelColor(
+                min(i + config.PIXEL_OFFSET, config.MAX_PIXEL_INDEX),
+                pixels[i]
+            )
+        self.show()
+        self._prev_pixels = pixels
+
+
+def RGB(r=0, g=0, b=0):
+    """ Converts strange RBG to more common RGB """
+    return Color(r, b, g)
+
+
+# Connnect ws2811 LEDs
+strip = Strip(
+    config.MAX_PIXEL_INDEX,
+    config.LED_PIN,
+    config.LED_FREQ_HZ,
+    config.LED_DMA,
+    config.LED_INVERT,
+    config.BRIGHTNESS,
+    config.LED_CHANNEL,
+    ws.WS2811_STRIP_GRB
+)
+strip.begin()
+
+strip.set_pixels([RGB(255, 0, 0) for i in range(config.N_PIXELS)])
