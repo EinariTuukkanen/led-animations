0ca0ff6ea60cfb96ea74430e04ba0b4ea7b76824
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2019-06-28 00:59:26 +0300

    ripple testing

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import json
 import time
 import socket
+import random
 import colorsys
 
 import config as cfg
@@ -29,15 +30,47 @@ def set_colors(areas, colors):
 
 
 def rainbow():
-    hue = 0
+    off = 0
+    m = 100.0 / room.led_count
     while True:
-        if hue > 1:
-            hue = 0
-        (r, g, b) = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
-        R, G, B = int(255 * r), int(255 * g), int(255 * b)
-        set_colors([room.name], [[R, G, B]] * room.led_count)
-        hue += 0.01
+        if off > 1:
+            off = 0
+        colors = []
+        for i in range(room.led_count):
+            (r, g, b) = colorsys.hsv_to_rgb(i * m / 100 + off, 1.0, 1.0)
+            R, G, B = int(255 * r), int(255 * g), int(255 * b)
+            colors.append([R, G, B])
+        set_colors([room.name], colors)
+        off += 0.01
         time.sleep(0.01)
 
 
-rainbow()
+def ripple():
+    off = 0
+    m = 100.0 / room.led_count
+    s = 0
+    ss = 1
+    maxs = random.randint(10, 1000)
+    while True:
+        if off > 1:
+            off = 0
+        if off < 0:
+            off = 1
+        if s > maxs:
+            s = 0
+            ss = -ss
+            maxs = random.randint(10, 1000)
+        colors = []
+        for i in range(room.led_count):
+            j = i % 10
+            k = (1 - abs(round(j / 10) * 10 - j) / 10) ** 3
+            (r, g, b) = colorsys.hsv_to_rgb(i * m / 100 + off, 1.0, k)
+            R, G, B = int(255 * r), int(255 * g), int(255 * b)
+            colors.append([R, G, B])
+        set_colors([room.name], colors)
+        off += ss * 0.01
+        time.sleep(0.01)
+        s += 1
+
+
+ripple()
