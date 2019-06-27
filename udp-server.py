e27f82d842acba140818bc0d0c864db9d03dc981
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2019-06-27 22:28:57 +0300

    remove extra prints

--- udp-server.py
+++ udp-server.py
@@ -94,5 +94,4 @@ if __name__ == '__main__':
     while True:
         data, addr = sock.recvfrom(2**14)  # buffer size is 1024 bytes
         data = json.loads(data.decode('utf-8'))
-        print('received message:', data)
         system.set_area_colors_rgb(data['colors'], data['areas'])
