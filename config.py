a8401f6a5aa6c3f519e6018852a30def9a0654d7
Einari Tuukkanen <einari.tuukkanen@gmail.com>
2019-06-27 21:19:38 +0300

    init leds v2

--- config.py
+++ config.py
@@ -1,124 +1,32 @@
-"""Settings for audio reactive LED strip"""
-from __future__ import print_function
-from __future__ import division
-import os
 
-DEVICE = 'pi'
-"""Device used to control LED strip. Must be 'pi',  'esp8266' or 'blinkstick'
+# IP address of the server (UDP)
+UDP_IP = '127.0.0.1'
 
-'esp8266' means that you are using an ESP8266 module to control the LED strip
-and commands will be sent to the ESP8266 over WiFi.
+# Port number used for UDP socket communication
+UDP_PORT = 5005
 
-'pi' means that you are using a Raspberry Pi as a standalone unit to process
-audio input and control the LED strip directly.
+# GPIO pin connected to the LED strip pixels (must support PWM)
+LED_PIN = 18
 
-'blinkstick' means that a BlinkstickPro is connected to this PC which will be used
-to control the leds connected to it.
-"""
+# LED signal frequency in Hz (usually 800kHz)
+LED_FREQ_HZ = 800000
 
-if DEVICE == 'esp8266':
-    UDP_IP = '192.168.0.150'
-    """IP address of the ESP8266. Must match IP in ws2812_controller.ino"""
-    UDP_PORT = 7777
-    """Port number used for socket communication between Python and ESP8266"""
-    SOFTWARE_GAMMA_CORRECTION = False
-    """Set to False because the firmware handles gamma correction + dither"""
+# DMA channel used for generating PWM signal (try 5)
+LED_DMA = 5
 
-if DEVICE == 'pi':
-    USE_RPI_LED = True
-    USE_BLUETOOTH_LED = False
-    """Use bluetooth led strip?"""
-    BLUETOOTH_ADDRESS = '78:9C:E7:0B:2F:50'
-    # BLUETOOTH_ADDRESS = 'A4:C1:38:9A:84:9F'  # New
-    """IP address of single color bluetooth led strip controller"""
-    ADDRESS = '192.168.1.220'
-    """IP address of the raspberry pi (socket server)"""
-    PORT = 8089
-    """Port number used for socket communication"""
-    MAX_CONNECTIONS = 5
-    """Max socket connections"""
-    LED_PIN = 18
-    """GPIO pin connected to the LED strip pixels (must support PWM)"""
-    LED_FREQ_HZ = 800000
-    """LED signal frequency in Hz (usually 800kHz)"""
-    LED_DMA = 5
-    """DMA channel used for generating PWM signal (try 5)"""
-    BRIGHTNESS = 255
-    """Brightness of LED strip between 0 and 255"""
-    LED_INVERT = False
-    """Set True if using an inverting logic level converter"""
-    SOFTWARE_GAMMA_CORRECTION = False
-    """Set to True because Raspberry Pi doesn't use hardware dithering"""
-    LED_CHANNEL = 0
-    """set to '1' for GPIOs 13, 19, 41, 45 or 53"""
+# Brightness of LED strip between 0 and 255
+BRIGHTNESS = 255
 
-if DEVICE == 'blinkstick':
-    SOFTWARE_GAMMA_CORRECTION = True
-    """Set to True because blinkstick doesn't use hardware dithering"""
+# Set True if using an inverting logic level converter
+LED_INVERT = False
 
-USE_GUI = True
-"""Whether or not to display a PyQtGraph GUI plot of visualization"""
+# Set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_CHANNEL = 0
 
-DISPLAY_FPS = True
-"""Whether to display the FPS when running (can reduce performance)"""
-
-N_PIXELS = 70
-"""Number of pixels in the LED strip (must match ESP8266 firmware)"""
-
-PIXEL_OFFSET = 30
-"""Number of dead pixels in the beginning"""
-
-MAX_PIXEL_INDEX = 100
-"""To be used with offset to determine real pixel count"""
-
-GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'gamma_table.npy')
-"""Location of the gamma correction table"""
-
-MIC_RATE = 44100
-"""Sampling frequency of the microphone in Hz"""
-
-FPS = 60
-"""Desired refresh rate of the visualization (frames per second)
-
-FPS indicates the desired refresh rate, or frames-per-second, of the audio
-visualization. The actual refresh rate may be lower if the computer cannot keep
-up with desired FPS value.
-
-Higher framerates improve "responsiveness" and reduce the latency of the
-visualization but are more computationally expensive.
-
-Low framerates are less computationally expensive, but the visualization may
-appear "sluggish" or out of sync with the audio being played if it is too low.
-
-The FPS should not exceed the maximum refresh rate of the LED strip, which
-depends on how long the LED strip is.
-"""
-_max_led_FPS = int(((N_PIXELS * 30e-6) + 50e-6)**-1.0)
-assert FPS <= _max_led_FPS, 'FPS must be <= {}'.format(_max_led_FPS)
-
-MIN_FREQUENCY = 200
-"""Frequencies below this value will be removed during audio processing"""
-
-MAX_FREQUENCY = 12000
-"""Frequencies above this value will be removed during audio processing"""
-
-N_FFT_BINS = 12
-"""Number of frequency bins to use when transforming audio to frequency domain
-
-Fast Fourier transforms are used to transform time-domain audio data to the
-frequency domain. The frequencies present in the audio signal are assigned
-to their respective frequency bins. This value indicates the number of
-frequency bins to use.
-
-A small number of bins reduces the frequency resolution of the visualization
-but improves amplitude resolution. The opposite is true when using a large
-number of bins. More bins is not always better!
-
-There is no point using more bins than there are pixels on the LED strip.
-"""
-
-N_ROLLING_HISTORY = 2
-"""Number of past audio frames to include in the rolling window"""
-
-MIN_VOLUME_THRESHOLD = 1e-7
-"""No music visualization displayed if recorded audio volume below threshold"""
+# Configure areas for the led system
+AREAS = {
+    'olohuone': {
+        'start_index': 0,
+        'end_index': 100,
+    },
+}
---
+++ udp-server.py
@@ -0,0 +1,94 @@
+import json
+import socket
+
+from neopixel import ws, Adafruit_NeoPixel, Color
+
+import config as cfg
+
+
+class LedStrip(Adafruit_NeoPixel):
+    def __init__(self, first_index, last_index, pin, freq_hz=800000, dma=10,
+                 invert=False, brightness=255, channel=0,
+                 strip_type=ws.WS2811_STRIP_RGB):
+        self.first_index = first_index
+        self.last_index = last_index
+        self.num = last_index - first_index
+        super().__init__(
+            self.num, pin, freq_hz, dma, invert,
+            brightness, channel, strip_type
+        )
+        self.state = []
+        self.begin()
+
+    def set_pixel_colors(self, colors=[]):
+        # Reset colors with empty method call
+        if not colors:
+            colors = [Color(0, 0, 0)] * self.num
+
+        if len(colors) != self.num:
+            raise Exception(f'Got {len(colors)} colors for {self.num} pixels')
+
+        for i, color in enumerate(colors):
+            # TODO: skip update if state not changed
+            self.setPixelColor(self.first_index + colors[i])
+
+        self.show()
+        self.state = colors
+
+    def set_pixel_colors_rgb(self, colors_rgb=[]):
+        self.set_pixel_colors([Color(r, g, b) for (r, g, b) in colors_rgb])
+
+
+class LedSystem:
+    def __init__(self, areas={}):
+        self.areas = areas
+
+    def add_area(self, name, strip):
+        self.areas[name] = strip
+
+    def set_area_colors(self, colors=[], areas=[]):
+        # Without specified area, run for all areas
+        if not areas:
+            areas = self.areas.keys()
+
+        for name in areas:
+            if name not in self.areas:
+                raise Exception(f'Invalid area {name}')
+            self.areas[name].set_pixel_colors(colors)
+
+    def set_area_colors_rgb(self, colors_rgb=[], areas=[]):
+        self.set_area_colors(
+            [Color(r, g, b) for (r, g, b) in colors_rgb], areas
+        )
+
+
+UDP_IP = cfg.UDP_IP
+UDP_PORT = cfg.UDP_PORT
+
+sock = socket.socket(
+    socket.AF_INET,     # Internet
+    socket.SOCK_DGRAM   # UDP
+)
+sock.bind((UDP_IP, UDP_PORT))
+
+common_configs = [
+    cfg.LED_PIN,
+    cfg.LED_FREQ_HZ,
+    cfg.LED_DMA,
+    cfg.LED_INVERT,
+    cfg.BRIGHTNESS,
+    cfg.LED_CHANNEL,
+    ws.WS2811_STRIP_RGB
+]
+
+# Connnect ws2811 LEDs into one system
+system = LedSystem({
+    area: LedStrip(params['start_index'], params['end_index'], *common_configs)
+    for area, params in cfg.AREAS
+})
+
+while True:
+    data, addr = sock.recvfrom(1024)  # buffer size is 1024 bytes
+    data = json.loads(data)
+    print('received message:', data)
+    system.set_area_colors(data['areas'], data['colors'])
